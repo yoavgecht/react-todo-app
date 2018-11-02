33d3125e8ce5f3c787b9d08503a5f3f18aad8180
Annapurna13 <yoavgecht@gmail.com>
2018-11-02 22:17:00 +0200

    Updates

---
+++ precache-manifest.915e08946adcbd02c649efc45627096f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3fa4c21b6dbce96361",
+    "url": "/react-todo-app/static/css/main.8eadd34d.chunk.css"
+  },
+  {
+    "revision": "0e3fa4c21b6dbce96361",
+    "url": "/react-todo-app/static/js/main.0e3fa4c2.chunk.js"
+  },
+  {
+    "revision": "07fd18ea5c7643374392",
+    "url": "/react-todo-app/static/js/1.07fd18ea.chunk.js"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "e75b2b66a2224de4eb8d1e42e763ade2",
+    "url": "/react-todo-app/index.html"
+  }
+];
